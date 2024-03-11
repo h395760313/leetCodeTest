385b990abd1fd5cb22b51ee6d3971aa5eff45186
h395760313 <zhangqian2@yxt.wecom.work>
2024-03-11 10:15:33 +0800

    【左神-排序】：荷兰国旗问题（小左大右）

---
+++ Leetcode/src/main/java/com/leetcode/leftgod/sort/FlagOfTheNetherlands.java
@@ -0,0 +1,35 @@
+package com.leetcode.leftgod.sort;
+
+import com.alibaba.fastjson.JSON;
+import org.junit.Test;
+
+/**
+ * 荷兰国旗问题
+ * @author xiehongyu
+ * @date 2024/3/11 10:03
+ */
+public class FlagOfTheNetherlands {
+
+    public void flagOfTheNetherlands(int[] arr, int target){
+        int l = -1;
+        int r = 1;
+        while (r < arr.length) {
+            if (arr[r] <= target) {
+                swap(arr, ++l, r);
+            }
+            r++;
+        }
+        System.out.println(JSON.toJSONString(arr));
+    }
+
+    private void swap(int[] arr, int i, int j){
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
+
+    @Test
+    public void test() {
+        flagOfTheNetherlands(new int[]{8,5,3,9,4,7,1,2,6,0}, 5);
+    }
+}
