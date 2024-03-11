dd77f57999e9ee6376ecf34a048b547317b63b8e
h395760313 <zhangqian2@yxt.wecom.work>
2024-03-11 10:23:03 +0800

    【左神-排序】：荷兰国旗问题2（小左大右）

--- Leetcode/src/main/java/com/leetcode/leftgod/sort/FlagOfTheNetherlands.java
+++ Leetcode/src/main/java/com/leetcode/leftgod/sort/FlagOfTheNetherlands.java
@@ -5,12 +5,13 @@ import org.junit.Test;
 
 /**
  * 荷兰国旗问题
+ *
  * @author xiehongyu
  * @date 2024/3/11 10:03
  */
 public class FlagOfTheNetherlands {
 
-    public void flagOfTheNetherlands(int[] arr, int target){
+    public void flagOfTheNetherlands(int[] arr, int target) {
         int l = -1;
         int r = 1;
         while (r < arr.length) {
@@ -22,7 +23,21 @@ public class FlagOfTheNetherlands {
         System.out.println(JSON.toJSONString(arr));
     }
 
-    private void swap(int[] arr, int i, int j){
+    public void flagOfTheNetherlands2(int[] arr, int target) {
+        int l = -1;
+        int r = arr.length;
+        int i = 0;
+        while (i < r) {
+            if (arr[i] <= target) {
+                swap(arr, ++l, i++);
+            } else {
+                swap(arr, --r, i++);
+            }
+        }
+        System.out.println(JSON.toJSONString(arr));
+    }
+
+    private void swap(int[] arr, int i, int j) {
         arr[i] = arr[i] ^ arr[j];
         arr[j] = arr[i] ^ arr[j];
         arr[i] = arr[i] ^ arr[j];
@@ -30,6 +45,6 @@ public class FlagOfTheNetherlands {
 
     @Test
     public void test() {
-        flagOfTheNetherlands(new int[]{8,5,3,9,4,7,1,2,6,0}, 5);
+        flagOfTheNetherlands2(new int[]{8, 5, 3, 9, 4, 7, 1, 2, 6, 0}, 5);
     }
 }
