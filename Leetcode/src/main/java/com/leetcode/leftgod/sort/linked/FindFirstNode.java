3aeb9a65f63e7c19541544e56e7d49ed600d2cb4
h395760313 <zhangqian2@yxt.wecom.work>
2024-03-12 11:39:48 +0800

    【左神-链表】：寻找循环单链表的第一个循环节点

---
+++ Leetcode/src/main/java/com/leetcode/leftgod/sort/linked/FindFirstNode.java
@@ -0,0 +1,52 @@
+package com.leetcode.leftgod.sort.linked;
+
+import com.leetcode.common.ListNode;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 寻找循环单链表的第一个循环节点
+ * @author xiehongyu
+ * @date 2024/3/12 11:31
+ */
+public class FindFirstNode {
+
+    public ListNode findFirstNode1(ListNode head){
+        Set<ListNode> set = new HashSet<>();
+        ListNode cur = head;
+        while (cur != null) {
+            if (set.contains(cur)) {
+                return cur;
+            }
+            set.add(cur);
+            cur = cur.next;
+        }
+        return null;
+    }
+
+    public ListNode findFirstIntersectNode2(ListNode head){
+        // 快慢指针
+        ListNode n1 = head.next;
+        ListNode n2 = head.next.next;
+        // 找到第一次相遇节点
+        while (n1 != n2) {
+            n1 = n1.next;
+            n2 = n2.next.next;
+        }
+        // 快指针从头出发，两个指针同时一次一步，相遇即第一个环节点
+        n2 = head;
+        while (n1 != n2) {
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        return n1;
+    }
+
+
+    @Test
+    public void test() {
+        System.out.println();
+    }
+}
